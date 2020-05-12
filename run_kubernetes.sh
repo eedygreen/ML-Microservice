91962ef4a96c1df6247761c53c599ec0033407e8
eedygreen <idris.isah2@st.futminna.edu.ng>
2020-05-12 22:30:58 +0100

    Operationalize Machine Learning with Microservice

---
+++ run_kubernetes.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+dockerpath="eedy/opml"
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run opmldemo\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=opmldemo
+
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward opmldemo 8000:80
+
